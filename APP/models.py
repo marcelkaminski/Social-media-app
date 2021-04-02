1b1e91b1b5a7e861056e03f36fd3ace7086bab48
marcelkaminski <marcel.kaminski.999@gmail.com>
2021-04-02 18:59:04 +0200

    Add Post Model

--- APP/models.py
+++ APP/models.py
@@ -5,3 +5,17 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     pass
+
+class Post(models.Model):
+    id = models.AutoField(primary_key=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey("User", on_delete=models.CASCADE, related_name="posts")
+    content = models.TextField(blank=True, null=True)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "timestamp": self.timestamp.strftime("%b %d %Y, %I:%M %p"),
+            "author": self.author.username,
+            "content": self.body
+        }
