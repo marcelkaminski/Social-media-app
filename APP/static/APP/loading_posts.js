20988fb4c3651cae61b395bebf2f4a9dd9ae7c30
marcelkaminski <marcel.kaminski.999@gmail.com>
2021-04-08 18:14:25 +0200

    Upgrade REST API

--- APP/static/APP/loading_posts.js
+++ APP/static/APP/loading_posts.js
@@ -4,16 +4,30 @@ document.addEventListener('DOMContentLoaded', function() {
 
 function load_posts()
 {
-    fetch('/posts')
-    .then(response => response.json())
-    .then(posts => {
-        posts.forEach(element => {
-            var item = document.createElement("div");
-            var myJSON = JSON.stringify(element);
-            item.innerHTML = `<li>
-                ${myJSON}
-                </li>`;
-          document.querySelector("#posts").appendChild(item);
-        });
-    });
+var xmlhttp = new XMLHttpRequest();
+var url = "/posts";
+
+xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        var myArr = JSON.parse(this.responseText);
+        myFunction(myArr);
+    }
+};
+xmlhttp.open("GET", url, true);
+xmlhttp.send();
+
+function myFunction(arr) {
+    var out = "";
+    var i;
+    for(i = 0; i < arr.length; i++) {
+        out += `<li>${JSON.stringify(arr[i])}</li>`;
+    }
+    document.getElementById("posts").innerHTML = out;
+}
+}
+
+
+function like_post()
+{
+    console.log("LIKED");
 }
--- APP/static/APP/search.js
+++ APP/static/APP/search.js
@@ -1,23 +1,34 @@
 document.addEventListener('DOMContentLoaded', function() {
-    document.querySelector('form').onsubmit = function() {
+    document.querySelector('form').onsubmit = function(e) {
+        e.preventDefault();
         const name = document.querySelector('#user-name').value.toLowerCase();
         document.querySelector("#search-result").innerHTML = "";
+        var xmlhttp = new XMLHttpRequest();
+        var url = `/search/${name}`;
+        xmlhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                var myArr = JSON.parse(this.responseText);
+                myFunction(myArr);
+            }
+        };
+        xmlhttp.open("GET", url, true);
+        xmlhttp.send();
 
-        fetch(`search/${name}`)
-        .then(response => response.json())
-        .then(data  => {
-            data.forEach(element => {
-                var item = document.createElement("div");
-                var myJSON = JSON.stringify(element);
-                item.innerHTML = `<div>
-                    ${myJSON}
-                    </div><br>`;
-              document.querySelector("#search-result").appendChild(item);
-            });
-        })
-        .catch(error => {
-            console.log('Error:', error);
-        });
-        return false;
-    }
+        function myFunction(arr) {
+            var out = "";
+            var i;
+            if (Object.keys(arr[0]) == "message") 
+            {
+                out = "<h1>User not found</h1>"
+            } 
+            else 
+            {
+                for(i = 0; i < arr.length; i++) 
+                {
+                    out += `<li><a href="/profile/${arr[i]['username']}">${arr[i]['username']}</a></li>`;
+                }             
+            }
+            document.getElementById("search-result").innerHTML = out;
+        }
+        }
 });
