f19d96003cbd5d2b736a862dad1238510f077078
marcelkaminski <marcel.kaminski.999@gmail.com>
2021-05-09 18:50:09 +0200

    Add dynamic loading with react post component

--- APP/static/APP/index.js
+++ APP/static/APP/index.js
@@ -1,37 +1,65 @@
-document.addEventListener('DOMContentLoaded', function() {
-    
-    const postsBox = document.getElementById('posts-box');
+class Posts extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        posts: [],
+      };
+    }
+
+    updateState(url) {
+    console.log(url);
+    // get all entities - GET
+    fetch(`${url}`, {
+    "method": "GET",
+    })
+    .then(response => response.json())
+    .then(response => {
+    this.setState({
+        posts: [...this.state.posts, response[0], response[1], response[2]]
+    })
+    })
+    .catch(err => { console.log(err); 
+    });
+    }
+
+    render() {
+        return (
+            <table>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Author</th>
+                        <th>Content</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {this.state.posts && this.state.posts.map(post => {
+                        return <tr>
+                            <td>{post.id}</td>
+                            <td>{post.author}</td>
+                            <td>{post.content}</td>
+                        </tr>
+                    })}
+                </tbody>
+            </table>
+        );
+    }
+}
+
+    let domContainer = document.querySelector('#App');
+    var feed = ReactDOM.render(<Posts/>, domContainer);
+
     const spinerBox = document.getElementById('spinner-box');
     const loadBtn = document.getElementById('load-btn');
-    const loadBox = document.getElementById('loading-box');
     
     let visible = 3;
-
-    const likeBtn = `<div class='likes-box'>{% if user.is_authenticated %}{% if request.user in post.likes.all %}<form action={% url 'like' pk=post.id %} method="POST">{% csrf_token %}<input type="submit" value="Unlike"/></form>{% else %}<form action={% url 'like' pk=post.id %} method="post">{% csrf_token %}<input type="submit" value="Like"/></form>{% endif %}{% endif %}Likes: {{post.likes.count}}</div>`
     
     function load_posts()
     {
-    var xmlhttp = new XMLHttpRequest();
     var url = `/posts/${visible}`;
-    
-    xmlhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            var myArr = JSON.parse(this.responseText);
-            myFunction(myArr);
-        }
-    };
-    xmlhttp.open("GET", url, true);
-    xmlhttp.send();
-    
-    function myFunction(arr) {
-       arr.map(post =>{
-           postsBox.innerHTML += `<div class='card w-75'>
-           ${post.author} | ${post.timestamp}
-           <br>
-           ${post.content}
-           </div>`
-       });
-    }}
+    feed.updateState(url);
+
+    }
     
     loadBtn.addEventListener('click', ()=>{
         visible += 3;
@@ -43,5 +71,6 @@ document.addEventListener('DOMContentLoaded', function() {
         }, 500)
     });
     load_posts();
-});
+
+
 
